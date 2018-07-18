dcacbd100e7fd66bc8a8ee1a873fc53d8f9f6a87
Joanna Gora <joanna.gora@protonmail.com>
2018-07-18 15:19:19 +0100

    created index

--- students_controller.rb
+++ students_controller.rb
@@ -1,15 +1,22 @@
 require('sinatra')
 require('sinatra/contrib/all')
-require_relative('models/student')
-require_relative('models/house')
+require_relative('./models/student')
+require_relative('./models/house')
 also_reload('./models/*')
 
 # index
+get '/students' do
+  @students = Student.all
+  erb :index
 
+end
 # show
 
 # new
-
+get '/students/new' do
+  @houses = ["Slitherin", "Ravenclaw", "Griffindor", "Hufflepuff"]
+  erb :new
+end
 # create
 
 # edit
